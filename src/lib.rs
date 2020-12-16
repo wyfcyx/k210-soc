c0f2a18dbd2b88d2db6076ce9bb9428c3a548246
Yifan Wu <shinbokuow@163.com>
2020-12-16 10:57:56 +0800

    Update.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod dmac;
+pub mod fpioa;
+pub mod gpio;
+pub mod gpiohs;
+pub mod sleep;
+pub mod spi;
+pub mod sysctl;
+pub mod utils;
