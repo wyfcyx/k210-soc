ec614de7c11d01e946809d239aef41f91860216a
rez5427 <785369607@qq.com>
2022-08-01 13:25:47 +0800

    gpiohs

--- src/gpiohs.rs
+++ src/gpiohs.rs
@@ -4,6 +4,8 @@
 //use k210_hal::pac;
 use k210_pac as pac;
 
+use crate::gpio::direction;
+
 use super::gpio;
 use super::utils::{set_bit,get_bit};
 
@@ -22,6 +24,36 @@ pub fn set_direction(pin: u8, direction: gpio::direction) {
     }
 }
 
+pub fn set_drive_mode(pin: u8, mode: drive_mode) {
+    let io_number = fpioa::get_io_by_function((fpioa::function::GPIOHS0 as u8 + pin).into());
+
+    let p: fpioa::pull;
+    let dir: direction;
+
+    match mode {
+        drive_mode::GPIO_DM_INPUT => {
+            p = fpioa::pull::NONE;
+            dir = direction::INPUT;
+        }
+        drive_mode::GPIO_DM_INPUT_PULL_DOWN => {
+            p = fpioa::pull::DOWN;
+            dir = direction::INPUT;
+        }
+        drive_mode::GPIO_DM_INPUT_PULL_UP => {
+            p = fpioa::pull::UP;
+            dir = direction::INPUT;
+        }
+        drive_mode::GPIO_DM_OUTPUT => {
+            p = fpioa::pull::DOWN;
+            dir = direction::OUTPUT;
+        }
+        _ => panic!("GPIO drive mode is not supported")
+    }
+
+    fpioa::set_io_pull(io_number as usize, p);
+    set_pin_en(pin, dir == direction::OUTPUT);
+}
+
 /** Set output value for a GPIOHS pin */
 pub fn set_pin(pin: u8, value: bool) {
     unsafe {
@@ -29,6 +61,18 @@ pub fn set_pin(pin: u8, value: bool) {
         (*ptr)
             .output_val
             .modify(|r, w| w.bits(set_bit(r.bits(), pin, value)));
+    }
+}
+
+pub fn set_pin_en(pin: u8, value: bool) {
+    unsafe {
+        let ptr = pac::GPIOHS::ptr();
+        (*ptr)
+            .output_en
+            .modify(|r, w| w.bits(set_bit(r.bits(), pin, value)));
+            (*ptr)
+            .input_en
+            .modify(|r, w| w.bits(set_bit(r.bits(), pin, !value)));
     }
 }
 
